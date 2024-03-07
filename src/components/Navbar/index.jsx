032a29bd84a79201ad9eb0115b6d9c1039ecdd4a
Gabriel Santos <bieelss01@gmail.com>
2024-03-07 11:42:32 -0300

    Feat: Create navbar and index section

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import "./styles.css";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export default function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <header>
+      <nav>
+        <div className="nav-options">
+          <img src="https://picsum.photos/200/80" alt="" />
+          <a href="">Opção</a>
+          <a href="">Opção</a>
+          <a href="">Opção</a>
+          <a href="">Opção</a>
+        </div>
+        <div className="nav-socials">
+          <a className="icon" href=""><WhatsAppIcon/></a>
+          <a className="icon" href=""><InstagramIcon/></a>
+          <a className="button" href="">Meus Agendamentos</a>
+        </div>
+      </nav>
+    </header>
+  );
 }
